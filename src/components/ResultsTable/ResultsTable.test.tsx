e68c0bb4bf69e07505d2c8d1ad99bf3a4cdd822b
Yuri Nascimento <yuri.nascimento@ride.capital>
2022-08-22 15:08:35 -0300

    backbone for rank table

---
+++ src/components/ResultsTable/ResultsTable.test.tsx
@@ -0,0 +1,17 @@
+import {render, screen} from '@testing-library/react';
+import ResultsTable from "./ResultsTable";
+
+describe('<ResultsTable>', () => {
+    it("Displays the header 'Rank', 'Team', 'Points' and 'Members'", ()=>{
+        render(<ResultsTable />);
+        const rankHeader = screen.getByText("Rank");
+        const teamHeader = screen.getByText("Team");
+        const pointsHeader = screen.getByText("Points");
+        const membersHeader = screen.getByText("Members");
+
+        expect(rankHeader).toBeInTheDocument();
+        expect(teamHeader).toBeInTheDocument();
+        expect(pointsHeader).toBeInTheDocument();
+        expect(membersHeader).toBeInTheDocument();
+    })
+});
---
+++ src/components/ResultsTable/ResultsTable.tsx
@@ -0,0 +1,11 @@
+const ResultsTable = ()=>{
+    return <><div>
+        <p>Rank</p>
+        <p>Team</p>
+        <p>Points</p>
+        <p>Members</p>
+    </div></>
+}
+
+
+export default ResultsTable
