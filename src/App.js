71efd5c7394e2035e8fdeb3fab3f1428e194db5c
Yuri Nascimento <yuri.nascimento@ride.capital>
2022-08-26 15:59:17 -0300

    Adiciona barra branca atrás do header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import ResultsTable from "./components/ResultsTable/ResultsTable";
 import React, { useEffect, useState } from "react";
 import { getFormattedGroups } from "./services/gateway";
+import "./App.css";
 
 const App = () => {
   const [data, setData] = useState();
@@ -17,8 +18,10 @@ const App = () => {
 
   return (
     <>
-      <h1>Bilingual challenge - 2022 (1st semester)</h1>
-      <h2>Results</h2>
+      <div className={"page-header"}>
+        <h1>Bilingual challenge - 2022 (1st semester)</h1>
+        <h2>Results</h2>
+      </div>
       <ResultsTable data={data} />
     </>
   );
